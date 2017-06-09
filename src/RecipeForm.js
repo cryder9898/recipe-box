6c7aff245056bee30696f2828e4a61db7cc36ab7
Christopher Ryder <cryder9898@gmail.com>
2017-06-08 20:33:04 -0400

    moved Form components to new file RecipeForm

---
+++ src/RecipeForm.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Form,
+  FormGroup,
+  Button,
+  Input,
+  InputGroup,
+  InputGroupButton
+} from 'reactstrap';
+
+class RecipeForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      list: '',
+    }
+  }
+
+  handleChange = (event) => {
+    if (event.target.name === 'list') {
+      this.setState({list: event.target.value});
+    } else {
+      this.setState({name: event.target.value});
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(
+      this.state.name,
+      this.state.list
+    );
+  }
+
+  render() {
+    return (
+      <Form>
+        <FormGroup>
+          <Input
+            type='text'
+            name='name'
+            placeholder='Recipe Name'
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <InputGroup>
+            <Input
+              type='text'
+              name='list'
+              placeholder='Ingredients'
+              value={this.state.list}
+              onChange={this.handleChange}
+            />
+            <InputGroupButton>
+              <Button>+</Button>
+            </InputGroupButton>
+          </InputGroup>
+        </FormGroup>
+      </Form>
+    );
+  }
+}
+
+export default RecipeForm;
--- src/RecipeModal.js
+++ src/RecipeModal.js
@@ -3,42 +3,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
-  Form,
-  FormGroup,
   Button,
   Modal,
   ModalHeader,
   ModalBody,
   ModalFooter,
-  Input,
-  InputGroup,
-  InputGroupButton
 } from 'reactstrap';
+import RecipeForm from './RecipeForm';
 
 class RecipeModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      list: '',
-    }
-  }
-
-  handleChange = (event) => {
-    if (event.target.name === 'list') {
-      this.setState({list: event.target.value});
-    } else {
-      this.setState({name: event.target.value});
-    }
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.onSubmit(
-      this.state.name,
-      this.state.foodList
-    );
-  }
 
   render() {
     return (
@@ -49,31 +22,9 @@ class RecipeModal extends Component {
           </ModalHeader>
           <ModalBody>
           { this.props.addEdit ?
-            <Form>
-              <FormGroup>
-                <Input
-                  type='text'
-                  name='name'
-                  placeholder='Recipe Name'
-                  value={this.state.name}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-              <FormGroup>
-                <InputGroup>
-                  <Input
-                    type='text'
-                    name='list'
-                    placeholder='Ingredients'
-                    value={this.state.list}
-                    onChange={this.handleChange}
-                  />
-                  <InputGroupButton>
-                    <Button>+</Button>
-                  </InputGroupButton>
-                </InputGroup>
-              </FormGroup>
-            </Form>
+            <RecipeForm
+              onSubmit={this.props.onSubmit}
+            />
             :
             this.props.recipe.name
           }
@@ -95,7 +46,12 @@ class RecipeModal extends Component {
                 Delete
               </Button>
             }
-            <Button color="secondary" onClick={this.props.toggle}>Cancel</Button>
+            <Button
+              color="secondary"
+              onClick={this.props.toggle}
+            >
+              Cancel
+            </Button>
           </ModalFooter>
         </Modal>
       </div>
