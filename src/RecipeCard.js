d8e3f01840632aedf761369dce53d8361ff75fa3
Christopher Ryder <cryder9898@gmail.com>
2017-06-10 07:33:27 -0400

    fixed properly formatted lines

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -8,7 +8,12 @@ const RecipeCard = (props) => {
         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
         <CardBlock>
           <CardTitle>{props.recipe.name}</CardTitle>
-          <Button value={props.index} onClick={props.viewContents}>View</Button>
+          <Button
+            value={props.index}
+            onClick={props.viewContents}
+          >
+            View
+          </Button>
         </CardBlock>
       </Card>
     </div>
