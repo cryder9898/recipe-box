1876f7262fae0e4da8655ff53c71499b86548c8f
Christopher Ryder <cryder9898@gmail.com>
2017-06-12 22:26:13 -0400

    fixed localStorage usage

--- src/index.js
+++ src/index.js
@@ -6,10 +6,34 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 var recipes;
 
-if (typeof(Storage) !== "undefined") {
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
+    }
+}
+
+if (storageAvailable('localStorage')) {
   // Code for localStorage/sessionStorage.
   console.log(localStorage);
-  if (localStorage.length === 0) {
+  if (!localStorage.getItem('recipes')) {
     console.log('nothing stored!!!!');
     recipes = [
       {
@@ -25,7 +49,7 @@ if (typeof(Storage) !== "undefined") {
         ingredients: ['Pasta', 'celery', 'bacon', 'italian dressing', 'tomato'],
       }
     ];
-    localStorage.setItem('recipes', recipes);
+    localStorage.setItem('recipes', JSON.stringify(recipes));
 } else {
     console.log('localStorage  YESS!!');
     recipes = JSON.parse(localStorage.getItem('recipes'));
